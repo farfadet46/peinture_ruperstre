916e04da09e56cff8304c5a1b825a966c7aba898
farfadet46 <farfadet46@gmail.com>
2025-02-26 21:03:22 +0100

    Add files via upload
    modification du code pour simplifier l'ajout de textures. une variable X = X blocs dans le jeu

--- init.lua
+++ init.lua
@@ -1,25 +1,22 @@
-local mod_name = minetest.get_current_modname()
+-- 2025 (CC0) Farfadet46 --
 
-minetest.register_node(mod_name .. ":bloc0", {
-    description = "peinture rupestre 0",
-    drawtype = "node",
-    tiles ={"peinture0.png"},
-    paramtype = "light",
-    groups = {snappy = 3},
-})
+local mod_name = core.get_current_modname()
 
-minetest.register_node(mod_name .. ":bloc1", {
-    description = "peinture rupestre 1",
-    drawtype = "node",
-    tiles ={"peinture1.png"},
-    paramtype = "light",
-    groups = {snappy = 3},
-})
+-- Nombre d'images (ajuste cette valeur selon tes besoins)
+local num_images = 10  -- Par exemple, 10 blocs (modifie ce nombre en fonction de ton mod)
 
-minetest.register_node( mod_name .. ":bloc2", {
-    description = "peinture rupestre 2",
-    drawtype = "node",
-    tiles ={"peinture2.png"},
-    paramtype = "light",
-    groups = {snappy = 3},   
-})
+-- Fonction pour créer un bloc avec une image
+local function create_block(i)
+    core.register_node(mod_name .. ":bloc" .. i, {
+        description = "peinture rupestre " .. i,
+        drawtype = "node",
+        tiles = {"peinture" .. i .. ".png"},  -- Utilisation dynamique des textures
+        paramtype = "light",
+        groups = {cracky = 3, stone = 1},
+    })
+end
+
+-- Boucle pour créer les blocs avec les textures
+for i = 0, num_images - 1 do
+    create_block(i)  -- Crée le bloc pour l'image 'i'
+end
